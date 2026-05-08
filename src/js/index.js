7b147de79d84aada692922fd155e5d6cbeb7a14e
arfrhmshrtn <iw8628940@gmail.com>
2026-05-08 14:41:00 +0700

    memperbaiki navbar stiky

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
 const humbererMenu = document.getElementById("humberger-menu");
 const navbarMenu = document.querySelector(".nav-links");
 
+const navbarContainer = document.querySelector(".navbar-container");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 10) {
+    navbarContainer.classList.add("sticky");
+  } else {
+    navbarContainer.classList.remove("sticky");
+  }
+});
+
 humbererMenu.addEventListener("click", function () {
   console.log("clicked");
   navbarMenu.classList.toggle("active");
